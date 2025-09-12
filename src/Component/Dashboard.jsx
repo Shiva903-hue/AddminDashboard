f4fcc18615893641920ec306e5f3f0879d9c8a85
Janvi Jawhrani <janvijawhrani@gmail.com>
2025-09-12 17:16:42 +0530

    report change to janvi

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -22,7 +22,7 @@ export default function Dashboard() {
                 : "bg-white text-gray-700 border-gray-300 hover:bg-gray-50 hover:border-gray-400 hover:shadow-sm"
             }`}
           >
-            Reports
+            Janvi
           </button>
         </Link>
 
